6b9103a40480a7a8f577f1cfa8a266946c2f1898
Venkata Janapareddy <venkatajanapareddy9@gmail.com>
2025-05-10 21:33:48 -0500

    test: Add test file for formatting

---
+++ test-formatting.ts
@@ -0,0 +1,7 @@
+// This file has intentionally poor formatting to test the pre-commit hooks
+const testFunction = () => {
+  console.log('This is a poorly formatted file');
+  return true;
+};
+
+export default testFunction;
